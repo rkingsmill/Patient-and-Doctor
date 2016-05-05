4eb19bd0f68122bd8a1554ed282f87067463c519
RKing <rosalyn.kingsmill@gmail.com>
2016-05-05 11:07:15 -0400

    Initial Commit

---
+++ Patient and Doctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Patient and Doctor
+//
+//  Created by Rosalyn Kingsmill on 2016-05-05.
+//  Copyright © 2016 Rosalyn Kingsmill. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
